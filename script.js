07ae1db2531070ede323a0804cbcd3563c963f31
Brian Shig Eddow <brianshigeddow@Brians-MacBook-Air.local>
2020-03-01 15:41:37 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* function main() {
+
+	
+
+}
+
+$(document).ready(main); */
+
+var contactMe = document.getElementsByClassName("contactMe")[0];
+var contactForm = document.getElementById("contactForm");
+
+contactMe.addEventListener("click", function() {
+	
+})
