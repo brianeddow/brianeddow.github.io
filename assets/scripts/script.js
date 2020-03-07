5534f80159c709ac3bedb5a0080a10a406b77454
Brian Shig Eddow <brianshigeddow@Brians-MacBook-Air.local>
2020-03-07 15:43:19 -0800

    use basic implementation of AOS library

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,3 +7,18 @@
 $(document).ready(main); */
 
 /* nothing here yet */
+
+
+/*
+
+// ----- sample AOS event listeners ----- //
+
+document.addEventListener('aos:in', ({ detail }) => {
+  console.log('animated in', detail);
+});
+
+document.addEventListener('aos:out', ({ detail }) => {
+  console.log('animated out', detail);
+});
+
+*/
