c7b668a0604555b4fecc0b29f631a9f6531471cd
Brian Eddow <brian.eddow@gmail.com>
2020-10-10 21:20:39 -0700

    add typewriter effect to header text

---
+++ assets/scripts/typewriter.js
@@ -0,0 +1,13 @@
+new Typewriter('#greeting1', {
+  strings: ['Hi, I\'m Brian ...'],
+  autoStart: true,
+  loop: true,
+  skipAddStyles: true
+});
+
+new Typewriter('#greeting2', {
+  strings: ['Thanks for visiting my landing page'],
+  autoStart: true,
+  loop: true,
+  skipAddStyles: true
+});
